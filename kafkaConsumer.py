da77463da887bda271356d60e9a7161b5c729898
kingan <kingan@tcd.ie>
2016-05-19 12:16:45 +0000

    Initial Commit

--- kafkaConsumer.py
+++ kafkaConsumer.py
@@ -1,13 +1,28 @@
 from kafka import KafkaConsumer
+import pymongo
+from pymongo import MongoClient
+import sys
 
-class kafkaConsumer(self):
+class kafkaConsumer():
     def __init__(self):
         self.var = 1
         self.consumer = KafkaConsumer('first')
+#        self.client = MongoClient("172.31.46.29:27017")
+        self.client = MongoClient("localhost:27017")
+        self.db = self.client.datapipeline
 
     def runKafkaConsumer(self):
-        
+        try:
+            while(self.consumer.next()):
+                tmp = self.consumer.next()
+                self.db.data0.insert({"offset":tmp.offset,"partition":tmp.partition,"topic":tmp.topic,"value":tmp.value})
+        except:
+            sys.exit()
+
+    def checkMongo(self):
+        print self.db.data0.find().next()
 
 if __name__ == "__main__":
     k = kafkaConsumer()
     k.runKafkaConsumer()
+#    k.checkMongo()
